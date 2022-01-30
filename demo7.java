7f2aca9ef3e800b0a6f109e9c19f118e6f10a72f
prkolli <kollicse28@gmail.com>
2022-01-30 15:48:27 +0530

    Changed by Dev A in master branch

--- demo7.java
+++ demo7.java
@@ -1,5 +1,5 @@
 public class demo7{
-	//demo7 hello world
+	//demo7 hello world - Dev A has changed the file
 	public static void main(String args[]){
 		System.out.printls("Hello World from demo7")
 	}
